89113e9b3545ad746497e8ec9b744aa0e36a3fb3
TX <22110482@bjtu.edu.cn>
2024-12-19 16:13:31 +0800

    init

--- dataloaders/paired_dataset.py
+++ dataloaders/paired_dataset.py
@@ -5,6 +5,8 @@ import random
 import numpy as np
 
 from torch import nn
+import torch
+import math
 from torchvision import transforms
 from torch.utils import data as data
 import torch.nn.functional as F
@@ -93,3 +95,87 @@ class PairedCaptionDataset(data.Dataset):
 
     def __len__(self):
         return len(self.gt_list)
+
+
+class WaymoRepairDataset(data.Dataset):
+    def __init__(self,
+                 root_folders=None,
+                 tokenizer=None,
+                 null_text_ratio=0.5):
+        super().__init__()
+        self.crop_preproc = transforms.Compose([
+            transforms.RandomCrop((512, 512)),
+            transforms.RandomHorizontalFlip(),
+        ])
+
+        self.img_preproc = transforms.Compose([
+            transforms.ToTensor(),
+        ])
+
+        self.data_dir = root_folders
+        self.tokenizer = tokenizer
+        self.null_text_ratio = null_text_ratio
+        self.gt_dir = os.path.join(self.data_dir, 'gt')
+        self.lq_dir = os.path.join(self.data_dir, 'lq')
+        self.tag_dir = os.path.join(self.data_dir, 'tag')
+        self.gt_list = sorted(glob.glob(os.path.join(self.gt_dir, '*.png')))
+        self.lq_list = sorted(glob.glob(os.path.join(self.lq_dir, '*.png')))
+        self.tag_list = sorted(glob.glob(os.path.join(self.tag_dir, '*.txt')))
+
+        ram_mean = [0.485, 0.456, 0.406]
+        ram_std = [0.229, 0.224, 0.225]
+        self.ram_normalize = transforms.Normalize(mean=ram_mean, std=ram_std)
+
+    def __len__(self):
+        return len(self.gt_list)
+
+    def __getitem__(self, idx):
+        gt_path = self.gt_list[idx]
+        gt_img = Image.open(gt_path).convert('RGB')
+        gt_img = self.img_preproc(gt_img)
+
+        lq_path = self.lq_list[idx]
+        lq_img = Image.open(lq_path).convert('RGB')
+        lq_img = self.img_preproc(lq_img)
+
+        assert gt_img.shape == lq_img.shape
+
+        if random.random() < self.null_text_ratio:
+            tag = ''
+        else:
+            tag_path = self.tag_path_list[idx]
+            file = open(tag_path, 'r')
+            tag = file.read()
+            file.close()
+
+        img = torch.cat([gt_img, lq_img], dim=0)
+        if img.shape[1] < 512 or img.shape[2] < 512:
+            # 等比缩放
+            scale_factor = 512 / min(img.shape[1], img.shape[2])
+            new_height = math.ceil(img.shape[1] * scale_factor)
+            new_width = math.ceil(img.shape[2] * scale_factor)
+            img = F.resize(img, (new_height, new_width),
+                                 interpolation=transforms.InterpolationMode.BICUBIC)
+        img = self.crop_preproc(img)
+
+        gt_img, lq_img = img[:3, :, :], img[3:, :, :]
+
+        example = dict()
+        example["conditioning_pixel_values"] = lq_img.squeeze(0)
+        example["pixel_values"] = gt_img.squeeze(0) * 2.0 - 1.0
+        example["input_ids"] = self.tokenize_caption(caption=tag).squeeze(0)
+
+        lq_img = lq_img.squeeze()
+
+        ram_values = F.interpolate(lq_img.unsqueeze(0), size=(384, 384), mode='bicubic')
+        ram_values = ram_values.clamp(0.0, 1.0)
+        example["ram_values"] = self.ram_normalize(ram_values.squeeze(0))
+
+        return example
+
+    def tokenize_caption(self, caption=""):
+        inputs = self.tokenizer(
+            caption, max_length=self.tokenizer.model_max_length, padding="max_length", truncation=True, return_tensors="pt"
+        )
+
+        return inputs.input_ids
